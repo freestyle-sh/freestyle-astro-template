afee16ef05866ef4d47d3db4de8170f3309d3bc9
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2024-05-07 12:38:10 -0700

    replace counter with todo list

---
+++ src/cloudstate/todo-list.ts
@@ -0,0 +1,40 @@
+import { cloudstate } from "freestyle-sh";
+
+@cloudstate
+export class TheTodoList {
+  static id = "todo-list" as const;
+
+  items: TodoItem[] = [];
+
+  addItem(text: string) {
+    const item = new TodoItem(text);
+    this.items.push(item);
+    return item;
+  }
+
+  getItems() {
+    return this.items.map((item) => item.getItem());
+  }
+}
+
+@cloudstate
+export class TodoItem {
+  id = crypto.randomUUID();
+  completed = false;
+
+  constructor(public text: string) {
+    this.text = text;
+  }
+
+  getItem() {
+    return {
+      id: this.id,
+      text: this.text,
+      completed: this.completed,
+    };
+  }
+
+  toggleCompletion() {
+    this.completed = !this.completed;
+  }
+}
---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { useCloud } from "freestyle-sh";
+  import { TodoItem } from "../cloudstate/todo-list";
+
+  export let id: string;
+  export let text: string;
+  export let completed: boolean;
+
+  const todoItem = useCloud<typeof TodoItem>(id);
+</script>
+
+<div>
+  <input
+    type="checkbox"
+    bind:checked={completed}
+    on:change={() => {
+      todoItem.toggleCompletion();
+    }}
+  />
+  <span>{text}</span>
+</div>
